63c0a3a1e3df48547a1ce0aa1f7febc98d0f4e57
Haliava <grenka388@gmail.com>
2020-03-02 23:08:53 +0400

    better reflection once again

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ import settings
 
 TILE_SIZE = settings.TILE_SIZE
 
-idleL, idleR, RunningL, RunningR = [list() for _ in range(4)]
-idleLrev, idleRrev, RunningLrev, RunningRrev = [list() for _ in range(4)]
-
-animIdle, animRun = 0, 0
-
 
 class GameObject:
     def __init__(self):
@@ -39,20 +34,22 @@ class Player(pygame.sprite.Sprite, GameObject):
         self.rect = pygame.Rect(TILE_SIZE * x, TILE_SIZE * y, TILE_SIZE, TILE_SIZE)
 
     def idle(self, animIdle):
+        global stay, sum1
         coords = (int(self.rect.x), int(self.rect.y - 12))
         letter = 'R' if stay == 1 else 'L'
         if sum1 % 2 == 0:
-            exec(f'screen.blit(idle{letter}[animIdle // 5], coords)')
+            exec(f'screen.blit(additional_functions.idle{letter}[additional_functions.animIdle // 5], coords)')
         else:
-            exec(f'screen.blit(idle{letter}rev[animIdle // 5], coords)')
+            exec(f'screen.blit(additional_functions.idle{letter}rev[additional_functions.animIdle // 5], coords)')
 
     def run(self, animRun):
+        global left, right
         coords = (int(self.rect.x), int(self.rect.y - 12))
         letter = 'L' if left else 'R'
         if sum1 % 2 == 0 and (left or right):
-            exec(f'screen.blit(Running{letter}[{animRun} // 5], coords)')
+            exec(f'screen.blit(additional_functions.Running{letter}[{animRun} // 5], coords)')
         elif left or right:
-            exec(f'screen.blit(Running{letter}rev[{animRun} // 5], coords)')
+            exec(f'screen.blit(additional_functions.Running{letter}rev[{animRun} // 5], coords)')
 
     def move(self, x, y):
         old_x, old_y, w, h = [int(x) for x in self.rect]
@@ -124,6 +121,7 @@ class Ball(pygame.sprite.Sprite, GameObject):
         self.vy = 5
 
     def update(self, *args):
+        global ball_hit_sound, screen, main_mirror, level
         if self.collide_with_sth()[0]:
             ball_hit_sound.play()  # <-- звук здесь, его не терять
             is_collided_with_wall = self.collide_with_sth(Wall)
@@ -133,29 +131,41 @@ class Ball(pygame.sprite.Sprite, GameObject):
                 initial_ball_tile = (self.rect.center[0] // TILE_SIZE, self.rect.center[1] // TILE_SIZE)
                 ball_tile = ((self.rect.center[0] + self.vx) // TILE_SIZE, (self.rect.center[1] + self.vy) // TILE_SIZE)
                 for wall in walls:
+                    c = 0
                     if (wall.initial_x, wall.initial_y) == ball_tile:
-                        if abs(initial_ball_tile[0] - ball_tile[0]) == 1 and \
-                                abs(initial_ball_tile[1] - ball_tile[1]) != 1:
-                            self.vx *= -1
-                        elif abs(initial_ball_tile[1] - ball_tile[1]) == 1 and \
-                                abs(initial_ball_tile[0] - ball_tile[0]) != 1:
+                        if abs(initial_ball_tile[1] - ball_tile[1]) == 1:
                             self.vy *= -1
-                        elif abs(initial_ball_tile[1] - ball_tile[1]) == 1 and \
-                                abs(initial_ball_tile[0] - ball_tile[0]) == 1:
-                            # непридвиденные обстоятельства
-                            self.rect = pygame.Rect(TILE_SIZE * self.initial_x,
-                                                    TILE_SIZE * self.initial_y,
-                                                    self.r, self.r)
-                            self.vx = 0
-                            self.vy = 5
+                            c += 1
+                        if abs(initial_ball_tile[0] - ball_tile[0]) == 1:
+                            self.vx *= -1
+                            c += 1
+                    if c == 2:
+                        i, j = initial_ball_tile[0], initial_ball_tile[1]
+                        try:
+                            horizontal, vertical = False, False
+                            if level[j - 1][i] == '#' or level[j + 1][i] == '#':
+                                vertical = True
+                            if level[j][i - 1] == '#' or level[j][i + 1] == '#':
+                                horizontal = True
+                            if horizontal:
+                                self.vy *= -1
+                            elif vertical:
+                                self.vx *= -1
+                        except IndexError:
+                            if j >= len(level) or j <= 0:
+                                self.vy *= -1
+                                break
+                            if i >= len(level) or i <= 0:
+                                self.vx *= -1
+                                break
             elif is_collided_with_mirror[0]:
-                self.reflect()
+                self.reflect(main_mirror)
         self.rect.x += self.vx
         self.rect.y += self.vy
         pygame.draw.circle(screen, pygame.Color('yellow'), (self.rect.x, self.rect.y), self.r)
 
-    def reflect(self):
-        angle = int(main_mirror.angle)
+    def reflect(self, mirror):
+        angle = int(mirror.angle)
         if player.direction == 1:
             num = 5
         else:
@@ -171,8 +181,9 @@ class Ball(pygame.sprite.Sprite, GameObject):
 
 
 class Mirror(pygame.sprite.Sprite, GameObject):
-    def __init__(self):
+    def __init__(self, m_image):
         super().__init__(all_sprites, mirror)
+        self.m_image = m_image
         self.image = m_image
         self.angle = 0
         self.rect = self.image.get_rect()
@@ -188,7 +199,7 @@ class Mirror(pygame.sprite.Sprite, GameObject):
         rel_x, rel_y = mouse_x - self.rect.x, mouse_y - self.rect.y
         self.angle = (180 / math.pi) * -math.atan2(rel_y, rel_x)
         if not (self.angle < -50 or self.angle > 50):
-            self.image = pygame.transform.rotate(m_image, int(self.angle))
+            self.image = pygame.transform.rotate(self.m_image, int(self.angle))
         self.rect = self.image.get_rect()
         self.rect.x = player.rect.x
         if player.direction == 1:
@@ -222,62 +233,24 @@ def generate_level(level):
                 Exit(x, y)
             elif level[y][x] == '|':
                 Ball(x, y, TILE_SIZE // 3)
-    return new_player, x, y
-
-
-def animation():
-    global idleL, idleR, RunningL, RunningR
-    global idleLrev, idleRrev, RunningLrev, RunningRrev
-
-    # Бездействие
-    for i in range(18):
-        idleR.append(pygame.image.load(f'data/idleR/0_Reaper_Man_Idle_{str(i).rjust(3, "0")}.png'))
-    for i in range(18):
-        idleL.append(pygame.image.load(f'data/idleL/0_Reaper_Man_Idle_{str(i).rjust(3, "0")}.png'))
-
-    # Бег
-    for i in range(12):
-        RunningR.append(pygame.image.load(f'data/RunningR/0_Reaper_Man_Running_{str(i).rjust(3, "0")}.png'))
-    for i in range(12):
-        RunningL.append(pygame.image.load(f'data/RunningL/0_Reaper_Man_Running_{str(i).rjust(3, "0")}.png'))
-
-    # Бездействие ревёрс
-    for i in range(18):
-        idleRrev.append(pygame.image.load(f'data/idleRrev/0_Reaper_Man_Idle_{str(i).rjust(3, "0")}.png'))
-    for i in range(18):
-        idleLrev.append(pygame.image.load(f'data/idleLrev/0_Reaper_Man_Idle_{str(i).rjust(3, "0")}.png'))
-
-    # Бег ревёрс
-    for i in range(12):
-        RunningRrev.append(pygame.image.load(f'data/RunningRrev/0_Reaper_Man_Running_{str(i).rjust(3, "0")}.png'))
-    for i in range(12):
-        RunningLrev.append(pygame.image.load(f'data/RunningLrev/0_Reaper_Man_Running_{str(i).rjust(3, "0")}.png'))
-
-    for elem in [idleR, idleL, RunningL, RunningR, idleRrev, idleLrev, RunningLrev, RunningRrev]:
-        im(elem)
-
-
-def im(arr):
-    for i in range(len(arr)):
-        arr[i] = arr[i].convert()
-        arr[i].set_colorkey((255, 255, 255))
+    return new_player, x, y, level
 
 
 def draw():
-    global animIdle, animRun
-    if animIdle >= 85:
-        animIdle = 0
-    if animRun >= 55:
-        animRun = 0
+    global left, right
+    if additional_functions.animIdle >= 85:
+        additional_functions.animIdle = 0
+    if additional_functions.animRun >= 55:
+        additional_functions.animRun = 0
     if left or right:
-        player.run(animRun)
-        animRun += 1
+        player.run(additional_functions.animRun)
+        additional_functions.animRun += 1
     else:
-        player.idle(animIdle)
-        animIdle += 1
+        player.idle(additional_functions.animIdle)
+        additional_functions.animIdle += 1
 
 
-def pause():
+def pause(screen):
     global player, level_x, level_y
     time_0 = time.time()
     screen.fill(pygame.Color('black'))
@@ -301,7 +274,7 @@ def pause():
             break
 
 
-def win():
+def win(screen):
     global player, level_x, level_y
     time_0 = time.time()
     screen.fill(pygame.Color('black'))
@@ -321,7 +294,8 @@ def win():
                     if button.rect.collidepoint(event.pos):
                         settings.CURRENT_LEVEL = f'lvl{button.text}'
                         pygame.quit()
-                        import main
+                        from main import game_loop
+                        game_loop()
             keys = pygame.key.get_pressed()
             if keys[pygame.K_ESCAPE] and time.time() - time_0 > 0.25:
                 break
@@ -336,99 +310,104 @@ mirror = pygame.sprite.Group()
 balls = pygame.sprite.Group()
 buttons = pygame.sprite.Group()
 
-pygame.init()
-screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGHT))
-clock = pygame.time.Clock()
-ball_hit_sound = pygame.mixer.Sound('data/ball_hit.wav')
-pygame.mixer.music.load('data/ambiance_loop.wav')
-pygame.mixer.music.set_volume(settings.MUSIC_VOLUME)
-ball_hit_sound.set_volume(settings.VOLUME)
-player, level_x, level_y = generate_level(additional_functions.load_level(f'{settings.CURRENT_LEVEL}.txt'))
-m_image = additional_functions.load_image('mim.jpg')
-main_mirror = Mirror()
 
-animation()
-time_from_last_esc = time.time() - 0.5
+def game_loop():
+    global left, right, main_mirror, stay, player, sum1, screen, ball_hit_sound, level
+    pygame.init()
+    screen = pygame.display.set_mode((settings.WIDTH, settings.HEIGHT))
+    clock = pygame.time.Clock()
+    ball_hit_sound = pygame.mixer.Sound('data/ball_hit.wav')
+    pygame.mixer.music.load('data/ambiance_loop.wav')
+    pygame.mixer.music.set_volume(settings.MUSIC_VOLUME)
+    ball_hit_sound.set_volume(settings.VOLUME)
+    player, level_x, level_y, level = generate_level(additional_functions.load_level(f'{settings.CURRENT_LEVEL}.txt'))
+    m_image = additional_functions.load_image('mim.jpg')
+    main_mirror = Mirror(m_image)
 
-left, right = False, False
-shift_pressed = False
-running = True
-pygame.mixer.music.play(-1)
-sum1, stay = 0, 0
+    additional_functions.animation()
+    time_from_last_esc = time.time() - 0.5
 
-while True:
-    screen.fill(pygame.Color('aquamarine'))
-    draw()
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            sys.exit()
-        if e.type == pygame.MOUSEBUTTONDOWN:
-            if e.button == 1 and player.ready_to_change_gravity:
-                sum1 += 1
-                player.change_gravity()
-        elif e.type == pygame.KEYDOWN:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.KMOD_LSHIFT]:
-                shift_pressed = True
-        elif e.type == pygame.KEYUP:
-            if e.key == pygame.KMOD_LSHIFT:
-                shift_pressed = False
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT] or \
-            keys[pygame.K_d] and not keys[pygame.K_a]:
-        stay = 1
-        left = False
-        right = True
-        player.move(10, 0)
-    elif keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT] or \
-            keys[pygame.K_a] and not keys[pygame.K_d]:
-        stay = 0
-        left = True
-        right = False
-        player.move(-10, 0)
-    elif keys[pygame.K_ESCAPE] and time.time() - time_from_last_esc > 0.25:
-        pause()
-        time_from_last_esc = time.time()
-    else:
-        left = False
-        right = False
+    left, right = False, False
+    shift_pressed = False
+    pygame.mixer.music.play(-1)
+    sum1, stay = 0, 0
 
-    all_sprites.update()
-    mirror.draw(screen)
-    for elem in player_group:
-        res = elem.collide_with_sth()
-        if res[0]:
-            if res[1] in (Wall, Exit):
-                elem.rect.x += 10 if elem.rect.x <= TILE_SIZE else -10
-            elif res[1] == Spike:
-                background_image = pygame.transform.scale(
-                    additional_functions.load_image('death_screen.jpg'), (settings.WIDTH, settings.HEIGHT)
-                )
-                screen.fill(pygame.Color('black'))
-                screen.blit(background_image, (0, -150))
-                restart_button = additional_functions.Button(200, 50, (11, 9), 'Заново', buttons, screen)
-                exit_button = additional_functions.Button(200, 50, (11, 11), 'Выход', buttons, screen)
-                while True:
-                    for event in pygame.event.get():
-                        if event.type == pygame.MOUSEBUTTONDOWN:
-                            if exit_button.rect.collidepoint(event.pos):
-                                from menu import start
+    while True:
+        screen.fill(pygame.Color('aquamarine'))
+        draw()
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                sys.exit()
+            if e.type == pygame.MOUSEBUTTONDOWN:
+                if e.button == 1 and player.ready_to_change_gravity:
+                    sum1 += 1
+                    player.change_gravity()
+            elif e.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.KMOD_LSHIFT]:
+                    shift_pressed = True
+            elif e.type == pygame.KEYUP:
+                if e.key == pygame.KMOD_LSHIFT:
+                    shift_pressed = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_RIGHT] and not keys[pygame.K_LEFT] or \
+                keys[pygame.K_d] and not keys[pygame.K_a]:
+            stay = 1
+            left = False
+            right = True
+            player.move(10, 0)
+        elif keys[pygame.K_LEFT] and not keys[pygame.K_RIGHT] or \
+                keys[pygame.K_a] and not keys[pygame.K_d]:
+            stay = 0
+            left = True
+            right = False
+            player.move(-10, 0)
+        elif keys[pygame.K_ESCAPE] and time.time() - time_from_last_esc > 0.25:
+            pause(screen)
+            time_from_last_esc = time.time()
+        else:
+            left = False
+            right = False
 
-                                start()
-                            elif restart_button.rect.collidepoint(event.pos):
-                                pygame.quit()
-                        elif event.type == pygame.MOUSEMOTION:
-                            screen.fill(pygame.Color('black'))
-                            rel = event.rel
-                            screen.blit(background_image, (rel[0] * 0.2, rel[1] * 0.2 - 150))
-                    buttons.update()
-                    clock.tick(settings.FPS)
-                    pygame.display.flip()
-    for elem in balls:
-        if elem.collide_with_sth(Exit)[0]:
-            win()
-    if shift_pressed:
-        clock.tick(30)
-    else:
-        clock.tick(settings.FPS)
-    pygame.display.flip()
+        all_sprites.update()
+        mirror.draw(screen)
+        for elem in player_group:
+            res = elem.collide_with_sth()
+            if res[0]:
+                if res[1] in (Wall, Exit):
+                    elem.rect.x += 10 if elem.rect.x <= TILE_SIZE else -10
+                elif res[1] == Spike:
+                    background_image = pygame.transform.scale(
+                        additional_functions.load_image('death_screen.jpg'), (settings.WIDTH, settings.HEIGHT)
+                    )
+                    screen.fill(pygame.Color('black'))
+                    screen.blit(background_image, (0, -150))
+                    restart_button = additional_functions.Button(200, 50, (11, 9), 'Заново', buttons, screen)
+                    exit_button = additional_functions.Button(200, 50, (11, 11), 'Выход', buttons, screen)
+                    while True:
+                        for event in pygame.event.get():
+                            if event.type == pygame.MOUSEBUTTONDOWN:
+                                if exit_button.rect.collidepoint(event.pos):
+                                    from menu import start
+                                    start()
+                                elif restart_button.rect.collidepoint(event.pos):
+                                    from main import game_loop
+                                    game_loop()
+                            elif event.type == pygame.MOUSEMOTION:
+                                screen.fill(pygame.Color('black'))
+                                rel = event.rel
+                                screen.blit(background_image, (rel[0] * 0.2, rel[1] * 0.2 - 150))
+                        buttons.update()
+                        clock.tick(settings.FPS)
+                        pygame.display.flip()
+        for elem in balls:
+            if elem.collide_with_sth(Exit)[0]:
+                win(screen)
+        if shift_pressed:
+            clock.tick(30)
+        else:
+            clock.tick(settings.FPS)
+        pygame.display.flip()
+
+
+game_loop()
